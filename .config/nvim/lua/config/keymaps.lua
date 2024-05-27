1ed3991576c964e4d8c809bfdb5d258a22ad92bb
andcj <sterrior@gmail.com>
2024-05-27 21:31:14 +0200

    Combined nvim settings from mac

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local cmp = require("cmp")
+
+-- Example keymap
+-- Set jk to escape
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+return {
+  ["<Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif require("luasnip").expand_or_jumpable() then
+      vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+    -- elseif vim.b._copilot_suggestion ~= nil then
+    --   vim.fn.feedkeys(vim.api.nvim_replace_termcodes(vim.fn["copilot#Accept"](), true, true, true), "")
+    else
+      fallback()
+    end
+  end, {
+    "i",
+    "s",
+  }), -- Example keymap
+  -- vim.keymap.set("n", "<leader>h", ':echo "Hello"<CR>', { noremap = true, silent = true }),
+  -- Set jk to escape
+  -- vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true }),
+}
--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -34,5 +34,122 @@ return {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = { style = "night" },
-  }
+  },
+
+  { "Shatur/neovim-ayu" },
+  { "simrat39/rust-tools.nvim" },
+  -- Hardtime
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+    {
+      {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+          { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+          { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+        },
+        opts = {
+          debug = true, -- Enable debugging
+          -- See Configuration section for rest
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+      },
+    },
+  },
+  {
+    "oxfist/night-owl.nvim",
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "night-owl",
+    },
+  },
+  -- change color of the cursorline
+  vim.cmd("highlight CursorLine guibg=#135500"),
+  -- vim.cmd("highlight CursorLine ctermbg=235 guibg=#4b5263")
+  -- change cursor type
+  vim.cmd(
+    "set guicursor=n-v-c:block,i-ci-ve:ver30,r-cr:hor20,o:hor50,a:blinkwait175-blinkoff150-blinkon175-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+  ),
+  -- hls search
+  vim.cmd("set hlsearch"),
+
+  -- Setup rust-tools
+  vim.cmd("let g:rustfmt_autosave = 1"),
+
+  -- Setup prettier
+  vim.cmd("nnoremap gp :silent %!prettier --stdin-filepath %<CR>"),
+  -- Don't format on save
+  vim.cmd("let g:prettier#autoformat = 0"),
+
+  -- cursor surrounding lines
+  vim.cmd("set scrolloff=100"),
+
+  -- Leader t to save
+  vim.cmd("nnoremap <leader>t :w<CR>"),
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
 }
