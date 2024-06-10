d8ac4c528b9deafab8d0125d67605f740b12d443
andcj <sterrior@gmail.com>
2024-06-11 00:35:44 +0200

    Playing with colors

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,10 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 -- config.color_scheme = "tokyonight_night"
-config.color_scheme = "Night Owl (Gogh)"
+-- config.color_scheme = "niji"
+-- config.color_scheme = "Numix Darkest (terminal.sexy)"
+config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
+-- config.color_scheme = "Dracula"
 
 config.font = wezterm.font("Monaspace Xenon", {
 	weight = 300,
@@ -21,5 +24,8 @@ config.hide_tab_bar_if_only_one_tab = true
 -- start full screen
 config.initial_rows = 50
 
+-- local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
+-- config.colors = theme.colors()
+-- window_frame = theme.window_frame()
 -- and finally, return the configuration to wezterm
 return config
