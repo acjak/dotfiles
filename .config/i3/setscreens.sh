738d4568a5de57a4ffc463a6fe86f32ce01f433d
Anders <sterrior@gmail.com>
2024-08-26 21:29:03 +0200

    Adding refresh rate

--- .config/i3/setscreens.sh
+++ .config/i3/setscreens.sh
@@ -4,11 +4,13 @@
 hostname=$(hostname)
 
 if [ "$hostname" = "anders-minipc" ]; then
-	xrandr \
-		--output DisplayPort-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
-		--output HDMI-A-0 --mode 2560x1440 --right-of DisplayPort-0 --rotate left
+  xrandr \
+    --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+    --output HDMI-A-0 --mode 2560x1440 --right-of DisplayPort-0 --rotate left
 elif [ "$hostname" = "anders-linux" ]; then
-	xrandr \
-		--output DP-4 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
-		--output DP-2 --mode 2560x1440 --right-of DP-4 --rotate right
+  xrandr \
+    --output DP-4 --primary --mode 2560x1440 --pos 0x0 --rate 144 --rotate normal \
+    --output DP-2 --mode 2560x1440 --right-of DP-4 --rotate right
+  # Set refresh rate of 144Hz
+  # xrandr --output DP-4 --mode 2560x1440 --rate 144
 fi
